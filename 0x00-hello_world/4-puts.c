d43bd302ff009e3808777f31ce5a577f2c554105
somanie1 <chisomonyiwa@gmail.com>
2023-04-20 11:55:37 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#Include stdio.h
+/**
+ * main - A program that prints a line with puts function
+ * Return: # (Success)
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
