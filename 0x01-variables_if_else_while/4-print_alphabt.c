51e48f31fb890be7e21ccdb2200b460605d83b36
somanie1 <chisomonyiwa@gmail.com>
2023-04-22 11:43:00 -0700

    4 print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+	       	if (letter != 'e' && letter != 'q')
+                        putchar(letter);
+        }
+
+        putchar('\n');
+
+        return (0);
+}
