e3a6a70d90fc78383b3ae16f570b1e3a0b50dc12
somanie1 <chisomonyiwa@gmail.com>
2023-04-22 13:08:20 -0700

    2 print alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,11 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        char letter;
-
-        for (letter='a'; letter<='z'; letter++)
+	char letter;
+	for (letter='a'; letter<='z'; letter++)
 		putchar(letter);
-
-        putchar('\n');
-
-        return(0);
+	putchar('\n');
+	return(0);
 }
