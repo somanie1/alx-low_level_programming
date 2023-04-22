9ab26635bf92be33faaf73d98a22bd4845d8836d
somanie1 <chisomonyiwa@gmail.com>
2023-04-22 13:03:21 -0700

    2 print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -12,10 +11,10 @@ int main(void)
 {
         char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-                putchar(letter);
+        for (letter='a'; letter<='z'; letter++)
+		putchar(letter);
 
         putchar('\n');
 
-        return (0);
+        return(0);
 }
