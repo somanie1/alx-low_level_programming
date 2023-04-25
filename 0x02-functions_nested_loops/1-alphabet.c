61b690857aecf69f36ae915f7d24db5e0cc6e8ac
somanie1 <chisomonyiwa@gmail.com>
2023-04-25 06:39:48 -0700

    1 alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,11 @@
  */
 void print_alphabet(void)
 {
-	int i;
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+        int i;
+        for (i = 97; i <= 122; i++)
+        {
+                _putchar(i);
+        }
+        _putchar('\n');
 }
+
