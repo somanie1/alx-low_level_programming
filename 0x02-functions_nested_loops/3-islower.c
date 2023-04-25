53e46c934c1a0b6a87afd22ca025a2a5b7ed6955
somanie1 <chisomonyiwa@gmail.com>
2023-04-25 15:33:57 -0700

    3 is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,17 @@
-#inclde "main.h"
+#include "main.h"
+
 /**
- * _islower - Shows 1 if the input is a
- * lowercase character. Another cases, shows
- * 0
- *
- * @c: The character in ASCII code
- *
- * Return: 1 for lowercase character. 0 for the rest.
+ * _islower - Checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 for lowercase character or 0 for anything else
  */
+
 int _islower(int c)
+
 {
 	if (c >= 97 && c <= 122)
 	{
-		return (1);
+	return (1);
 	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+	return (0);
 }
