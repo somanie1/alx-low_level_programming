2582800c926010d8a383e22c6f9aef74655becd9
somanie1 <chisomonyiwa@gmail.com>
2023-04-27 02:24:52 -0700

    Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,15 @@
+{
+	int 1;
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
