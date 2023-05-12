9569e1de054017fb58628a64edccb79b881ea60a
somanie1 <chisomonyiwa@gmail.com>
2023-05-12 11:27:42 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int _attribute((unused_)) argc, char *argv[]);
+int main(int argc, char _attribute((unused_)) *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int _putchar(char c);
+
+#endif
